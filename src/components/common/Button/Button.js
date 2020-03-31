4c9aee2f0e694820fdca4f934417a9af95781d55
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-03-31 21:55:24 +0200

    add sign in page

---
+++ src/components/common/Button/Button.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
+
+const Btn = ({
+    buttonId,
+    type,
+    className,
+    onClick,
+    children,
+    loading,
+    ...props
+}) => (
+        <Button
+            id={buttonId}
+            type={type}
+            className={className}
+            onClick={onClick}
+            loading={loading}
+            {...props}
+        >
+            {children}
+        </Button>
+    );
+Btn.propTypes = {
+    buttonId: PropTypes.string,
+    type: PropTypes.string,
+    loading: PropTypes.bool,
+    children: PropTypes.any,
+    onClick: PropTypes.func.isRequired
+};
+
+Btn.defaultProps = {
+    type: 'button',
+    children: 'button',
+    onClick: e => e,
+    loading: false
+};
+
+export default Btn;
---
+++ src/components/common/Raindrop/Raindrop.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+import './Raindrop.scss';
+
+const FarmImage = require('../../../assets/images/field.jpg');
+const FarmPhone = require('../../../assets/images/phone.png');
+const Facebook = require('../../../assets/images/facebook.png');
+const Twitter = require('../../../assets/images/twitter.png');
+const Youtube = require('../../../assets/images/youtube.png');
+const Instagram = require('../../../assets/images/instagram.png');
+
+const RaindropImage = ({ children }) => {
+    return (
+        <div>
+            <div className="container">
+                <Image src={FarmImage} className="image-farm" alt="farm" />
+                <p className="raindrop-title">Dial *515# to Access Innovation Agriculture</p>
+                <p className="raindrop-title2">Services on MLima wherever you are.</p>
+                <div className="phone-container">
+                    <Image src={FarmPhone} id="raindrop-phone" alt="phone" />
+                    <div className="rounded-cicle">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
+
+            </div>
+            <div className="social-images">
+                <Image className="social" src={Facebook} alt="facebook" />
+                <Image className="social" src={Instagram} alt="instagram" />
+                <Image className="social" src={Youtube} alt="youtube" />
+                <Image className="social" src={Twitter} alt="twitter" />
+            </div>
+            {children}
+        </div>
+    )
+}
+
+export default RaindropImage;
