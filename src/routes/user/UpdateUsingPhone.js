8d18a1d888567536673a3be977a39217bb0ee151
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-04-04 21:54:05 +0200

    add reset password pages

---
+++ src/routes/user/UpdateUsingPhone.js
@@ -0,0 +1,9 @@
+import UpdatePasswordPhone from '../../components/PasswordReset/UpdateUsingPhone';
+
+export default {
+    exact: true,
+    name: 'Update Password',
+    protected: false,
+    path: '/update-with-phone',
+    component: UpdatePasswordPhone
+}
