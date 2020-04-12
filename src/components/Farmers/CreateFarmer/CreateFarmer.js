d462d4be1f921f5fe60482a06f2adfd9c4ba7cde
Jean luc <luctunechi45@gmail.com>
2020-04-13 00:25:17 +0200

    add new farmer

--- src/components/Farmers/CreateFarmer/CreateFarmer.js
+++ src/components/Farmers/CreateFarmer/CreateFarmer.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import UserSidebar from '../../common/Sidebar/Sidebar';
-import Navbar from '../../common/Navbar/Navbar';
-import { MiniNavbar } from '../../common/Navbar/MiniNavbar';
-import CreateFarmerForm from './CreateFarmerForm';
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+import UserSidebar from "../../common/Sidebar/Sidebar";
+import Navbar from "../../common/Navbar/Navbar";
+import { MiniNavbar } from "../../common/Navbar/MiniNavbar";
+import CreateFarmerForm from "./CreateFarmerForm";
 
-import '../../../assets/css/table.scss';
+import "../../../assets/css/table.scss";
 
 class CreateFarmer extends Component {
-    render() {
-        return (
-            <div className="bg-container">
-                <Navbar />
-                <MiniNavbar home="Home" settings={<>Farmers</>} users={<><span className="blue-color">New</span></>} />
-                <Grid>
-                    <Grid.Column width={3}>
-                        <UserSidebar />
-                    </Grid.Column>
-                    <Grid.Column className="style-role" width={13}>
-                        <CreateFarmerForm />
-                    </Grid.Column>
-                </Grid>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className='bg-container'>
+				<Navbar />
+				<MiniNavbar
+					home='Home'
+					settings={<>Farmers</>}
+					users={
+						<>
+							<span className='blue-color'>New </span>
+						</>
+					}
+				/>
+				<Grid>
+					<Grid.Column width={3}>
+						<UserSidebar />
+					</Grid.Column>
+					<Grid.Column className='style-role' width={13}>
+						<CreateFarmerForm />
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	}
 }
 
 export default CreateFarmer;
--- src/components/Farmers/CreateFarmer/CreateFarmerForm.js
+++ src/components/Farmers/CreateFarmer/CreateFarmerForm.js
@@ -1,82 +1,201 @@
-import React, { Component } from 'react';
-import { Card, Grid, Button, Form, Select } from 'semantic-ui-react';
-import SemanticDatepicker from 'react-semantic-ui-datepickers';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Card, Grid, Button, Form, Select, Input } from "semantic-ui-react";
+import SemanticDatepicker from "react-semantic-ui-datepickers";
 
-import { zone } from '../../../helpers/userRegistration/zone';
-import '../../../assets/css/table.scss';
+import { zone } from "../../../helpers/userRegistration/zone";
+import { site } from "../../../helpers/userRegistration/site";
+import { provinces } from "../../../helpers/userRegistration/provinces";
+import { gender } from "../../../helpers/userRegistration/gender.js";
+import { districts } from "../../../helpers/userRegistration/districts";
+import { sectors } from "../../../helpers/userRegistration/sectors";
+import { village } from "../../../helpers/userRegistration/villages";
+import { cell } from "../../../helpers/userRegistration/cell";
 
-
-const options = [
-    { key: 'm', text: 'Male', value: 'male' },
-    { key: 'f', text: 'Female', value: 'female' },
-    { key: 'o', text: 'Other', value: 'other' },
-]
+import "../../../assets/css/scroll.scss";
+import "../../../assets/css/table.scss";
+import "../../../assets/css/scroll.scss";
+import "./CreateFarmer.scss";
 
 class CreateFarmerForm extends Component {
-    render() {
-        return (
-            <>
-                <Card.Group className="table-card">
-                    <Card fluid>
-                        <Card.Content className="header-bg-color">
-                            Farmer's registration form
-                        </Card.Content>
-                        <Grid>
-                            <Grid.Column floated='left' width={8}>
-                                <Form>
-                                    <Form.Field>
-                                        <label>First Name</label>
-                                        <input placeholder='First Name' />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <label>Last Name</label>
-                                        <input placeholder='Last Name' />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <label>ID number</label>
-                                        <input placeholder='ID number' />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Form.Select
-                                            fluid
-                                            label='Gender'
-                                            options={options}
-                                            placeholder='Gender'
-                                        />
-                                    </Form.Field>
-                                    <Form.Group widths="equal">
-                                        <SemanticDatepicker
-                                            label="Date of birth"
-                                            name="birth"
-                                        />
-                                    </Form.Group>
-                                    <Form.Field>
-                                        <label>Nationality</label>
-                                        <input placeholder='nationality' />
-                                    </Form.Field>
-                                    <Form.Field
-                                        id="select"
-                                        control={Select}
-                                        label="Zone"
-                                        options={zone}
-                                        placeholder="Zone...."
-                                        name="zone"
-                                        search
-                                    />
-                                    <Button type='submit'>Submit</Button>
-                                </Form>
-                            </Grid.Column>
-                            <Grid.Column floated='right' width={8}>
+	state = {
+		file: null,
+	};
 
-                            </Grid.Column>
-                        </Grid>
-                    </Card>
-                </Card.Group>
-                <p id="footer-content">Copyright &copy; MAHWI Tech Ltd</p>
-            </>
-        );
-    }
+	fileInputRef = React.createRef();
+
+	fileChange = (e) => {
+		this.setState({ file: e.target.files[0] }, () => {
+			console.log("File chosen --->", this.state.file);
+		});
+	};
+
+	render() {
+		return (
+			<>
+				<Card.Group className='table-card scroll-style'>
+					<Card fluid>
+						<Card.Content className='header-bg-color'>
+							Farmer's registration form
+						</Card.Content>
+						<Form>
+							<Grid>
+								<Grid.Column floated='left' width={8}>
+									<div
+										id='edit-scroll'
+										className='left-padding'
+									>
+										<Form.Field>
+											<label>First Name</label>
+											<input placeholder='First Name' />
+										</Form.Field>
+										<Form.Field>
+											<label>Last Name</label>
+											<input placeholder='Last Name' />
+										</Form.Field>
+										<Form.Field>
+											<label>ID number</label>
+											<input placeholder='ID number' />
+										</Form.Field>
+										<Form.Field>
+											<Form.Select
+												fluid
+												label='Gender'
+												options={gender}
+												placeholder='Gender'
+											/>
+										</Form.Field>
+										<Form.Group widths='equal'>
+											<SemanticDatepicker
+												label='Date of birth'
+												name='birth'
+											/>
+										</Form.Group>
+										<Form.Field>
+											<label>Nationality</label>
+											<input placeholder='nationality' />
+										</Form.Field>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Zone'
+											options={zone}
+											placeholder='Zone....'
+											name='zone'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Site'
+											options={site}
+											placeholder='Site....'
+											name='Site'
+											search
+										/>
+									</div>
+								</Grid.Column>
+								<Grid.Column floated='right' width={8}>
+									<div
+										id='edit-scroll'
+										className='left-padding'
+									>
+										<Form.Field>
+											<label>Phone No</label>
+											<input placeholder='Phone Number' />
+										</Form.Field>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Province'
+											options={provinces}
+											placeholder='Province....'
+											name='province'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='District'
+											options={districts}
+											placeholder='District....'
+											name='district'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Sector'
+											options={sectors}
+											placeholder='Sector....'
+											name='sector'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Cell'
+											options={cell}
+											placeholder='Cell....'
+											name='cell'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Village'
+											options={village}
+											placeholder='Village....'
+											name='village'
+											search
+										/>
+										<Form.Field
+											id='select'
+											control={Select}
+											label='Zone'
+											options={zone}
+											placeholder='Zone....'
+											name='zone'
+											search
+										/>
+										<Form.Field>
+											<label>Membership fees</label>
+											<input placeholder='Membership fees' />
+										</Form.Field>
+										<Form.Field>
+											<label>Profile Picture</label>
+											<Input
+												icon='upload'
+												content='Choose File'
+												className='icon-upload'
+												onClick={() =>
+													this.fileInputRef.current.click()
+												}
+												placeholder='Upload Image..'
+											/>
+											<input
+												ref={this.fileInputRef}
+												type='file'
+												hidden
+												onChange={this.fileChange}
+											/>
+										</Form.Field>
+										<Button
+											primary
+											className='btn-create-farmer'
+											type='submit'
+										>
+											Save
+										</Button>
+									</div>
+								</Grid.Column>
+							</Grid>
+						</Form>
+					</Card>
+				</Card.Group>
+				<p id='footer-content'>Copyright &copy; MAHWI Tech Ltd</p>
+			</>
+		);
+	}
 }
 
 export default CreateFarmerForm;
