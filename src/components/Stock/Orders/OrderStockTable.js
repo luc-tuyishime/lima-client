10882a92bda814a6f2c50b0d08472e6ed5ce5803
Jean luc <luctunechi45@gmail.com>
2020-04-29 00:59:25 +0200

    add new changes

--- src/components/Stock/Orders/OrderStockTable.js
+++ src/components/Stock/Orders/OrderStockTable.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Card, Table, Search, Grid, Button, Icon, Popup, Label } from 'semantic-ui-react';
+import { Card, Table, Search, Grid, Button, Icon, Popup, Label, Modal } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import OrderModal from './OrderModal';
 import '../../../assets/css/table.scss';
+import './orders.scss';
 
 class OrderStockTable extends Component {
 
@@ -15,7 +16,7 @@ class OrderStockTable extends Component {
     close = () => this.setState({ open: false })
 
     render() {
-         const { open, size } = this.state
+        const { open, size } = this.state
         return (
             <>
                 <Card.Group className="table-card">
@@ -33,9 +34,6 @@ class OrderStockTable extends Component {
                                         placeholder="Search..."
                                     />
                                 </Grid.Column>
-                                <Grid.Column floated='right' width={2}>
-                                <OrderModal />
-                                </Grid.Column>
                             </Grid>
 
                             <Table className="table-card" singleLine>
@@ -50,13 +48,13 @@ class OrderStockTable extends Component {
                                 </Table.Header>
 
                                 <Table.Body>
-                                    <Table.Row>
+                                    <Table.Row className="table-row-style" onClick={this.show()}>
                                         <Table.Cell><Label color='red' horizontal>New</Label></Table.Cell>
                                         <Table.Cell>September 14, 2013</Table.Cell>
                                         <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                     <Table.Row>
                                         <Table.Cell><Label color='red' horizontal>New</Label></Table.Cell>
@@ -64,7 +62,7 @@ class OrderStockTable extends Component {
                                         <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
                                         <Table.Cell>Yes</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                     <Table.Row>
                                         <Table.Cell><Label color='red' horizontal>New</Label></Table.Cell>
@@ -72,7 +70,7 @@ class OrderStockTable extends Component {
                                         <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
                                         <Table.Cell>Yes</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                     <Table.Row>
                                         <Table.Cell><Label color='red' horizontal>New</Label></Table.Cell>
@@ -80,7 +78,7 @@ class OrderStockTable extends Component {
                                         <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
                                         <Table.Cell>Yes</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                     <Table.Row>
                                         <Table.Cell></Table.Cell>
@@ -88,7 +86,7 @@ class OrderStockTable extends Component {
                                         <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
                                         <Table.Cell>Yes</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                     <Table.Row>
                                         <Table.Cell></Table.Cell>
@@ -96,13 +94,74 @@ class OrderStockTable extends Component {
                                         <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
                                         <Table.Cell>Yes</Table.Cell>
                                         <Table.Cell>No</Table.Cell>
-                                        
+
                                     </Table.Row>
                                 </Table.Body>
                             </Table>
                         </Card.Content>
                     </Card>
                 </Card.Group>
+
+                {/* MODAL */}
+                <Modal size={size} open={open} onClose={this.close}>
+                  <Modal.Header>Order Number</Modal.Header>
+                    <Modal.Content>
+                    <Grid>
+                        <Grid.Column floated='left' width={5}>
+                        <p>Name: Lastname Firstname</p>
+                        <p>Phone number: +250784421255</p>
+                        <p>Date: dd-mm-yyyy</p>
+                        </Grid.Column>
+                    </Grid>
+                    </Modal.Content>
+                    <Table className="table-order" singleLine>
+                    <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Category</Table.HeaderCell>
+                                <Table.HeaderCell>Product name</Table.HeaderCell>
+                                <Table.HeaderCell>Quantity</Table.HeaderCell>
+                                <Table.HeaderCell>Stock available</Table.HeaderCell>
+                                <Table.HeaderCell>Unit for measure</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell>Equipment</Table.Cell>
+                                <Table.Cell>September 14, 2013</Table.Cell>
+                                <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+                                <Table.Cell>No</Table.Cell>
+                                <Table.Cell>No</Table.Cell>
+                                
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>Equipment</Table.Cell>
+                                <Table.Cell>January 11, 2014</Table.Cell>
+                                <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+                                <Table.Cell>Yes</Table.Cell>
+                                <Table.Cell>No</Table.Cell>
+                                
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>Equipment</Table.Cell>
+                                <Table.Cell>May 11, 2014</Table.Cell>
+                                <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+                                <Table.Cell>Yes</Table.Cell>
+                                <Table.Cell>No</Table.Cell>
+                                
+                            </Table.Row>
+                        </Table.Body>
+                        </Table>
+                        <Modal.Actions>
+                            <Button negative>Reject</Button>
+                            <Button
+                                positive
+                                icon='checkmark'
+                                labelPosition='right'
+                                content='Approve'
+                            />
+                    </Modal.Actions>
+                    </Modal>
                 <p id="footer-content">Copyright &copy; MAHWI Tech Ltd</p>
             </>
         );
